ecf073798d80223d929ef0710921538e28237bb7
bhserna <bhserna@gmail.com>
2015-04-09 11:34:17 -0500

    Modeling data

--- js/application.js
+++ js/application.js
@@ -3,3 +3,8 @@ window.Todos = Ember.Application.create();
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
 });
+
+Todos.Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
+});

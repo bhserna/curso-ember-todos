a77c9d1dd16ea9049e6c698dc3a21c0964d8e390
bhserna <bhserna@gmail.com>
2015-04-09 11:43:38 -0500

    Creating a new model instance

--- js/application.js
+++ js/application.js
@@ -12,6 +12,28 @@ Todos.TodosRoute = Ember.Route.extend({
   }
 });
 
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function() {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', '');
+
+      // Save the new model
+      todo.save();
+    }
+  }
+});
+
 Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')

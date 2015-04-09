feab71d9e7f6119f375fa574229a1dfe04e76510
bhserna <bhserna@gmail.com>
2015-04-09 11:35:29 -0500

    Using fixtures

--- js/application.js
+++ js/application.js
@@ -1,5 +1,7 @@
 window.Todos = Ember.Application.create();
 
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' });
 });
@@ -8,3 +10,21 @@ Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
+
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
